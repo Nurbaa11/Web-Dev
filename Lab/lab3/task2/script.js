6fd705a77aa3949c75a1ebfed7a64737986c7390
Nurba11 <khodjabergen@gmail.com>
2026-02-24 17:57:49 +0500

    commit

--- Lab/lab3/task2/script.js
+++ Lab/lab3/task2/script.js
@@ -1,1 +1,43 @@
 
+const form = document.getElementById('todoForm');
+const input = document.getElementById('taskInput');
+const list = document.getElementById('taskList');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const text = input.value.trim();
+  if (text === '') return;
+
+  const li = document.createElement('li');
+  li.className = 'item';
+
+  const left = document.createElement('div');
+  left.className = 'left';
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+
+  const span = document.createElement('span');
+  span.textContent = text;
+
+  checkbox.addEventListener('change', function () {
+    span.classList.toggle('done', checkbox.checked);
+  });
+
+  const delBtn = document.createElement('button');
+  delBtn.type = 'button';
+  delBtn.textContent = 'Delete';
+  delBtn.addEventListener('click', function () {
+    list.removeChild(li);
+  });
+
+  left.appendChild(checkbox);
+  left.appendChild(span);
+
+  li.appendChild(left);
+  li.appendChild(delBtn);
+
+  list.appendChild(li);
+  input.value = '';
+});
